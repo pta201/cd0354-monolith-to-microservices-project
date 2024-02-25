5b77bb944695bedea548147dda41898d6cd81fd3
hminh2027 <dackheros@gmail.com>
2024-02-25 21:26:40 +0700

    trigger build

--- set_env.sh
+++ set_env.sh
@@ -7,5 +7,5 @@ export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::test-364374721048-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=thisismysecret
 export URL=http://localhost:8100
